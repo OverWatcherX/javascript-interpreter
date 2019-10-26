db65ca6f9377cd98e64d5508c1f1a8b3fea069cc
brambles <qjnight@gmail.com>
2019-10-27 01:56:24 +0800

    feat: 修复发布脚本

--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ git diff --exit-code \
     && git diff --cached --exit-code \
     && npm run build \
     && git checkout gh-pages \
-    && cp -rf _book/* . 
     && git pull \
+    && cp -rf _book/* . \
     && git commit -a -m "publish: ${date}" \
     && git push \
     && git checkout master

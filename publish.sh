7f886e0ad5e9abb0f7a2b436033bc2576b8aeab7
brambles <qjnight@gmail.com>
2019-10-27 01:47:45 +0800

    chore: 修改发布脚本

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ git diff --exit-code \
     && git diff --cached --exit-code \
     && npm run build \
     && git checkout gh-pages \
-    && mv _book/* _book/.* . 
+    && cp -rf _book/* _book/.* . 
     && git pull \
     && git commit -a -m "publish: ${date}" \
     && git push \

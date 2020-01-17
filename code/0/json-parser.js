cf06312ef0ad1d86f4026ee554ed8f342b1755a2
brambles <qjnight@gmail.com>
2020-01-17 17:49:03 +0800

    book: 准备第一章

---
+++ code/0/json-parser.js
@@ -0,0 +1,19 @@
+const jison = require("jison");
+const fs = require('fs').promises;
+const path = reuqire('path');
+
+const Parser = jison.Parser;
+
+function __main__() {
+}
+
+// const =;
+// const inject = function(file, context)
+
+// const context = {
+//   function() {
+//   },
+// };
+
+// function inject 
+
